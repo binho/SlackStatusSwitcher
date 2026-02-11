31e9238dfe47a6845465c250558feb4d1a86df80
Cleber Santos <cleber@metalab.com>
2026-02-10 23:17:06 -0300

    Initial Commit

---
+++ SlackStatusSwitcher/SlackStatusSwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlackStatusSwitcherApp.swift
+//  SlackStatusSwitcher
+//
+//  Created by Cleber Santos on 2/10/26.
+//
+
+import SwiftUI
+
+@main
+struct SlackStatusSwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
